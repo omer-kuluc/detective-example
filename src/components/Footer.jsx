3a471c72daba32f9f7de679ac778f0039782ec21
omer-kuluc <omerkuluc96@gmail.com>
2026-02-19 03:35:49 +0300

    footer jsx düzenlendi

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 export default function Footer() {
   return (
     <footer className="main-footer">
-      {/* Üst kısımdaki ince cyan çizgi efekti */}
       <div className="footer-glow-line"></div>
 
       <div className="footer-copyright">
